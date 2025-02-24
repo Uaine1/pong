e9550f3155fdf72e5874152a055bd7aa8e599fb3
Uaine1 <angeloalcantara243@gmail.com>
2025-02-24 14:45:55 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pong
+Pong
+Pong
+Pong
+Pong
+Pong
+Pong 
+
+## Covered Topics
+- 
+
+## Tutorial Used
+Here's the link for the youtube video that i used to practice and all assets used belongs to Clear Code:<br>
+#### Clear Code's Pygame Tutorial: <br> https://youtu.be/8OMghdHP-zs?si=9a6baVcOE9La2XEa
---
+++ code/main.py
@@ -0,0 +1,40 @@
+from settings import *
+from sprites import *
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.screen_display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.running = True
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Pong")
+        
+        # Sprites
+        self.all_sprites = pygame.sprite.Group()
+        self.paddle_sprite = pygame.sprite.Group()
+        self.player = Player((self.all_sprites,self.paddle_sprite))
+        self.ball = Ball(self.all_sprites, self.paddle_sprite)
+        
+        
+    def run(self):
+        while self.running:
+            dt = self.clock.tick() / 1000
+            
+            #Event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    
+            # Update
+            self.all_sprites.update(dt)
+            
+            # Draw screen
+            self.screen_display.fill(COLORS["bg"])
+            self.all_sprites.draw(self.screen_display)
+            pygame.display.update()
+            
+        pygame.quit()
+      
+if __name__ == "__main__":
+    game = Game()
+    game.run()
