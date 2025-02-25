e700828f7d7b429629111257b848b7e08c7a1f3e
Uaine1 <angeloalcantara243@gmail.com>
2025-02-25 13:31:33 +0800

    Add collision between moving objects

--- code/sprites.py
+++ code/sprites.py
@@ -9,6 +9,7 @@ class Player(pygame.sprite.Sprite):
         pygame.draw.rect(self.image, COLORS["paddle"], pygame.FRect((0,0), SIZE["paddle"]), 0, 10)
         self.rect = self.image.get_frect(center = POS["player"])
         self.speed = SPEED["player"]
+        self.old_rect = self.rect.copy()
         self.direction = 0
         
         
@@ -24,6 +25,7 @@ class Player(pygame.sprite.Sprite):
     
     
     def update(self, dt):
+        self.old_rect = self.rect.copy()
         self.move(dt)
         self.get_direction()
         
@@ -31,17 +33,21 @@ class Player(pygame.sprite.Sprite):
 class Ball(pygame.sprite.Sprite):
     def __init__(self, groups, paddle_sprites):
         super().__init__(groups)
+        self.paddle_sprites = paddle_sprites
+        
         self.image = pygame.Surface(SIZE["ball"], pygame.SRCALPHA)
         pygame.draw.circle(self.image, COLORS["ball"], (SIZE["ball"][0] / 2, SIZE["ball"][1] / 2), SIZE["ball"][0] / 2)
         
         self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-        
-        self.speed = SPEED["ball"]
+        self.old_rect = self.rect.copy()
         self.direction = pygame.Vector2(choice((1,-1)), uniform(0.7, 0.8) * choice((-1, 1)))
         
         
     def move(self, dt):
-        self.rect.center += self.direction * SPEED["ball"] * dt
+        self.rect.x += self.direction.x * SPEED["ball"] * dt
+        self.collision("horizontal")
+        self.rect.y += self.direction.y * SPEED["ball"] * dt
+        self.collision("vertical")
         
         
     def wall_collision(self):
@@ -60,8 +66,30 @@ class Ball(pygame.sprite.Sprite):
         if self.rect.left <= 0:
             self.rect.left = 0
             self.direction.x *= -1
+            
+            
+    def collision(self, direction):
+        for sprite in self.paddle_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if direction == "horizontal":
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left
+                        self.direction.x *= -1
+                        
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right
+                        self.direction.x *= -1
+                else:       
+                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
+                        self.rect.bottom = sprite.rect.top
+                        self.direction.y *= -1    
+                        
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom
+                        self.direction.y *= - 1          
 
         
     def update(self, dt):
+        self.old_rect = self.rect.copy()
         self.move(dt)
         self.wall_collision()
